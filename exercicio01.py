db50deeba6736b2265d88cd1641eff0284cd943b
Vinícius Xavier <vinix1508@gmail.com>
2023-08-01 09:59:22 -0300

    Feito os exercicio01.py e exercicio02.py

---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+v1 = float(input("Diga a sua primeira nota escolar: "))
+v2 = float(input("Diga a sua segunda nota escolar: "))
+v3 = float(input("Diga a sua terceiro nota escolar: "))
+
+media = (v1 + v2 + v3) / 3
+
+print("Sua média das notas: ", media)
---
+++ exercicio02.py
@@ -0,0 +1,6 @@
+num = int(input("Diga um número e lhe direi seu antecessor e o seu sucessor: "))
+
+ant = num - 1
+suc = num + 1
+
+print("O antecessor do seu número é", ant, "e o sucessor é", suc)
